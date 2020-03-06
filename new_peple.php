7ec292d780c4663cfb38673e86d2d229e123882e
木場聡一郎 <60096223+soichirokoba0910@users.noreply.github.com>
2020-03-06 15:18:46 +0900

    Update new_peple.php

--- new_peple.php
+++ new_peple.php
@@ -18,33 +18,12 @@
     <?php //IDとパスは何桁が望ましい？　パス2個目は確認用 ?>
     会員ID:<input type="text" name="id" value="<?php if(!empty($_POST['id'])) echo $_POST['id'];?>">
     <br>
-    パスワード:<input type="password" name="pass[]" value="">
+    パスワード:<input type="password" name="pass1" value="">
     <br>
-    確認用パスワード:<input type="passward" name="pass[]" value="">
-    <br>
+    確認用パスワード:<input type="password" name="pass2" value="">
+    <p>※パスワードは半角英数字をそれぞれ1文字以上含んだ8文字以上で設定してください</p>
     <input type="submit" name="submit" value="登録する" class="bottan">
     <input type="reset" name="reset" value="取り消し" class="bottan">
     </form>
-    <?php
-    if($_POST){
-      if(empty($_POST['name'])){
-        echo '<p>※名前を入力してください</p>';
-      }
-      if(empty($_POST['mail'])){
-        echo "<p>※メールアドレスを入力してください</p>";
-      }
-      if(empty($_POST['birth'])){
-        echo "<p>※生年月日を入力してください</p>";
-      }
-      if(empty($_POST['id'])){
-        echo "<p>※会員IDを入力してください</p>";
-      }
-      if(empty($_POST['pass'][0])||empty($_POST['pass'][1])){
-        echo "<p>※パスワードを入力してください</p>";
-      } elseif(!($_POST['pass'][0]==$_POST['pass'][1])){
-        echo "<p>※パスワードが異なります</p>";
-      }
-    }
-    ?>
   </body>
 </html>
