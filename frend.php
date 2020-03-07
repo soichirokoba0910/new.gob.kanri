0edb632eb88eb55c878f6e2bd107ae7ffb287cc8
木場聡一郎 <60096223+soichirokoba0910@users.noreply.github.com>
2020-03-08 01:45:16 +0900

    Update frend.php

--- frend.php
+++ frend.php
@@ -39,6 +39,7 @@
       $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       try{
+//参加者サーチ開始////////////////////////////////////////////////////////////////////////////////
         $sql="SELECT *FROM enter";
         $stmt = $dbh->query($sql);
         echo '<div class="hyou"><p>参加者一覧</p>';
@@ -60,8 +61,9 @@
         echo  $sanka;
       }
         echo '<br>計'.$enter.'人</div>';
-        //ここまで参加者
+//ここまで参加者サーチ欄//////////////////////////////////////////////////////////////////////////
 
+//不参加者サーチ開始//////////////////////////////////////////////////////////////////////////////
         $sql="SELECT *FROM no_enter";
         $stmt = $dbh->query($sql);
         echo '<div class="hyou"><p>不参加者一覧</p>';
@@ -84,8 +86,9 @@
         echo   $nosanka;
       }
       echo '計'.$no_enter.'人</div>'; 
-       //ここまで不参加者
+//ここまで不参加者サーチ欄////////////////////////////////////////////////////////////////////////
 
+//不明者サーと開始///////////////////////////////////////////////////////////////////////////////
        $sql="SELECT *FROM unkonow";
        $stmt = $dbh->query($sql);
        echo '<div class="hyou"><p>不明者一覧</p>';
@@ -108,8 +111,7 @@
        echo  $humei;
      }
      echo '計'.$unkown.'人</div>'; 
-     //ここまで不明者
-
+//ここまで不明者サーチ欄/////////////////////////////////////////////////////////////////////////
       } catch(PDOException $e){
         print('接続失敗:' . $e->getMessage());
         die();
@@ -124,45 +126,72 @@
       <input  class="bottan" type="submit" value="送信">
       <input  class="bottan" type="reset"  value="取り消し">
     </form>
-    <?php
+    <?php 
       if($_POST){
-        try{
         $dbh = new PDO('mysql:host=localhost;dbname=manabiya;charset=utf8', $user,$pass);
         $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try{
+        session_start();
+//参加者挿入開始///////////////////////////////////////////////////////////////////////     
         if($_POST['entry']==1){
-  
-            $sql ="INSERT INTO enter(name,id) VALUES (:name,:id)";
-            $stmt =$dbh->prepare($sql);
-            session_start();
-            $stmt->bindValue(':name',$_SESSION['name'],PDO::PARAM_STR);
-            $stmt->bindValue(':id',$_SESSION['id'],PDO::PARAM_STR);  
-            $stmt->execute(); 
-             echo "参加メンバーに加わりました";
-             $dbh=null;
-             //参加者一覧
-          
-        } elseif($_POST['entry']==2) {
-          $sql ="INSERT INTO no_enter(name,id) VALUES (:name,:id)";
+          $sql = "SELECT * FROM enter";
+          $stmt = $dbh->query($sql);
+          foreach($stmt as $row){
+            if($row['name_id'] == $_SESSION['nameid']);
+             echo "<p>※すでに参加者メンバーです</p>";
+             echo '<a href = "main_job.php">戻る</a>';
+             return false;
+          }
+          $sql ="INSERT INTO enter(name,id,name_id) VALUES (:name,:id,:name_id)";
           $stmt =$dbh->prepare($sql);
-          session_start();
           $stmt->bindValue(':name',$_SESSION['name'],PDO::PARAM_STR);
-          $stmt->bindValue(':id',$_SESSION['id'],PDO::PARAM_STR);  
+          $stmt->bindValue(':id',$_SESSION['id'],PDO::PARAM_STR);
+          $stmt->bindValue(':name_id',$_SESSION['nameid'],PDO::PARAM_STR);
+          $stmt->execute();
+           echo "参加メンバーに加わりました";
+           $dbh=null;
+//参加者曹宇乳終了/////////////////////////////////////////////////////////////////////
+
+//不参加者挿入開始/////////////////////////////////////////////////////////////////////              
+        } elseif($_POST['entry']==2) {
+          $sql = "SELECT * FROM no_enter";
+          $stmt = $dbh->query($sql);
+          foreach($stmt as $row){
+            if($row['name_id'] == $_SESSION['nameid']);
+             echo "<p>※すでに不参加者メンバーです</p>";
+             echo '<a href = "main_job.php">戻る</a>';
+             return false;
+          }
+          $sql ="INSERT INTO no_enter(name,id,name_id) VALUES (:name,:id,:name_id)";
+          $stmt =$dbh->prepare($sql);
+          $stmt->bindValue(':name',$_SESSION['name'],PDO::PARAM_STR);
+          $stmt->bindValue(':id',$_SESSION['id'],PDO::PARAM_STR);
+          $stmt->bindValue(':name_id',$_SESSION['nameid'],PDO::PARAM_STR);
           $stmt->execute(); 
           echo '不参加メンバーに加わりました';
            $dbh=null;
-           //不参加一覧
-      
+//不参加挿入終了///////////////////////////////////////////////////////////////////////
+
+//不明者挿入開始///////////////////////////////////////////////////////////////////////     
         } else {
-          $sql ="INSERT INTO unkonow(name,id) VALUES (:name,:id)";
+          $sql = "SELECT * FROM unkonow";
+          $stmt = $dbh->query($sql);
+          foreach($stmt as $row){
+            if($row['name_id'] == $_SESSION['nameid']);
+             echo "<p>※すでに不明者メンバーです</p>";
+             echo '<a href = "main_job.php">戻る</a>';
+             return false;
+          }
+          $sql ="INSERT INTO unkonow(name,id,name_id) VALUES (:name,:id,:name_id)";
           $stmt =$dbh->prepare($sql);
-          session_start();
           $stmt->bindValue(':name',$_SESSION['name'],PDO::PARAM_STR);
-          $stmt->bindValue(':id',$_SESSION['id'],PDO::PARAM_STR);  
+          $stmt->bindValue(':id',$_SESSION['id'],PDO::PARAM_STR);
+          $stmt->bindValue(':name_id',$_SESSION['nameid'],PDO::PARAM_STR);
           $stmt->execute(); 
-          echo '不明';
+          echo '不明者加わりました';
            $dbh=null;
-           //不明者一覧
+//不明者一覧///////////////////////////////////////////////////////////////////////////
         } 
       } catch(PDOException $e){
         print('接続失敗:' . $e->getMessage());
